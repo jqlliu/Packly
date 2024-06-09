26452b060cdb29205838be7f2333e635f234e5ec
AntSwift1 <antswift10@gmail.com>
2024-06-08 23:30:15 -0400

    Turned the inputs into a Form

--- src/app/app.login.ts
+++ src/app/app.login.ts
@@ -1,22 +1,28 @@
 import { Component } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import {FormGroup, FormControl} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 
 //This is a comment
 @Component({
   selector: 'app-login',
   template: `
-<label class="flex flex-col items-center">
- <p>E-Mail: <input id="email" type="email" class = "border"/></p>
- <p>Username: <input id="username" type="username" class = "border"/></p>
- <p>Password: <input id="password" type="password" class = "border"/></p>
- <button class = "border">Login</button>
-</label>
+<form class="flex flex-col items-center" [formGroup]="submissionForm">
+ <p>E-Mail: <input id="email" type="email" class = "border" formControlName = "email" name = "email"/></p>
+ <p>Username: <input id="username" type="username" class = "border" formControlName = "username" name = "username"/></p>
+ <p>Password: <input id="password" type="password" class = "border" formControlName = "password" name = "password"/></p>
+ <button type="submit" class = "border" [disabled]="!submissionForm.valid"> Login </button>
+</form>
 `,
   standalone: true,
-  imports: [ FormsModule ],
+  imports: [ FormsModule, ReactiveFormsModule],
   styleUrl: './app.component.css'
 })
 
 export class LoginComponent {
   title = 'Login';
+  submissionForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  })
 }
