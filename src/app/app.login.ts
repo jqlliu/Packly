57cdb63bbb71f20c85a6aea64d0684e810a4e50d
AntSwift1 <antswift10@gmail.com>
2024-06-08 22:50:11 -0400

    Modified data from copied home file

--- src/app/app.login.ts
+++ src/app/app.login.ts
@@ -1,19 +1,24 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 //This is a comment
 @Component({
-  selector: 'app-home',
+  selector: 'app-login',
   template: `
-Welcome to brazail
+<label for="framework">
+  E-Mail:
+  <input id="framework" type="text" />
+</label>
 `,
   standalone: true,
+  imports: [ FormsModule ],
   styleUrl: './app.component.css'
 })
 
 
 
-export class HomeComponent {
-  title = 'Home';
+export class LoginComponent {
+  title = 'Login';
 
 }
