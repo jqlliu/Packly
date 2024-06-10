7c1cc9c551c30eb69c9a739bd545255aa210ba15
AntSwift1 <antswift10@gmail.com>
2024-06-10 12:12:45 -0400

    Added routing to Signup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import {AppComponent} from './app.component';
 import { HomeComponent } from './app.home';
 import { PurchaseComponent } from './app.purchase';
 import {LoginComponent} from './app.login';
+import { SignupComponent } from './app.signup';
 //Next step add the db
 export const routes: Routes = [{
     path: '',
@@ -16,4 +17,8 @@ export const routes: Routes = [{
     path: 'login',
     title: 'Login',
    component: LoginComponent
+},{
+    path: 'signup',
+    title: 'Signup',
+   component: SignupComponent
 },];
