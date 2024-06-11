3b37c2e984d512dc14232efff5d206e2d3d58987
jqlliu <2260216963liu@gmail.com>
2024-06-11 10:51:53 -0400

    Lmao idk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { NgOptimizedImage } from '@angular/common';
   template: ` 
     <nav class = "bg-gray-500 py-2">
     <div class = "container flex mx-auto justify-between items-center">
-    <img ngSrc="../assets/img/logo.png" alt="image" width = "16" height = "16">
+    <img ngSrc="../img/logo.png" alt="image" width = "48" height = "48">
       <a href = "" class = "text-white text-2xl font-semibold">Logo</a>
       <ul class = "flex space-x-4 content-left basis-8/12">
         <li class = "text-white hover:text-gray-200"><a routerLink="/">Home</a></li>
