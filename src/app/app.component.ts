45d6c995b2fff4c1fd74ba469f721a422edfe972
jqlliu <2260216963liu@gmail.com>
2024-06-11 12:42:32 -0400

    Logo but better I think

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,11 @@ import { NgOptimizedImage } from '@angular/common';
 //Yoinked
 @Component({
   selector: 'app-root',
-  template: ` 
+  template: `   
     <nav class = "bg-gray-500 py-2">
-    <div class = "container flex mx-auto justify-between items-center">
-    <img ngSrc="../img/logo.png" alt="image" width = "48" height = "48">
-      <a href = "" class = "text-white text-2xl font-semibold">Logo</a>
+    <div class = "container flex mx-auto justify-around items-center">
+      <a href = "" class = "flex basis-1/12 text-white text-2xl font-semibold items-center">
+      <img ngSrc="../img/logoNew.png" alt="image" width = "48" height = "48">LMAO</a>
       <ul class = "flex space-x-4 content-left basis-8/12">
         <li class = "text-white hover:text-gray-200"><a routerLink="/">Home</a></li>
         <li class = "text-white hover:text-gray-200"><a routerLink="/purchase">Purchase</a></li>
