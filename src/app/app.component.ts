ddc0db7720401e9ae2b735075005d3514c62838a
AntSwift1 <antswift10@gmail.com>
2024-06-10 23:29:07 -0400

    Attempted to add image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { ApiService } from './api.service';
 import { OnInit } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 //Yoinked
 @Component({
   selector: 'app-root',
   template: `
 <nav class = "bg-gray-500 py-2">
     <div class = "container flex mx-auto justify-between items-center">
+    <img ngSrc="../assets/img/logo.png" alt="image" width = "16" height = "16">
       <a href = "" class = "text-white text-2xl font-semibold">Logo</a>
       <ul class = "flex space-x-4 content-left basis-8/12">
         <li class = "text-white hover:text-gray-200"><a routerLink="/">Home</a></li>
@@ -22,7 +24,7 @@ import { OnInit } from '@angular/core';
 <router-outlet />
 `,
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, NgOptimizedImage],
   styleUrl: './app.component.css'
 })
 
