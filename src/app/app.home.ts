bd8ae90e1571369273dbc15822d297d7a7e38b38
AntSwift1 <antswift10@gmail.com>
2024-06-13 01:53:07 -0400

    Added 2 extra text boxes

--- src/app/app.home.ts
+++ src/app/app.home.ts
@@ -6,8 +6,16 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-home',
   template: `
   <!-- This breaks if the description is too short, oops -->
-<div class = "flow-text break-words min-w-xl max-w-max rounded-lg border-2 border-black-500 px-4 py-2 mx-3 my-3 text-center">
-  This is a description. Here is another sentence. And another. And another. And another. And another. And another. And another. And another. And another. And another. And another. And another. And another. And another. And another. And another. And another. And another. And another. And another. And another. And another. And another.
+<div class = "flow-text break-words w-xl max-w-max rounded-lg border-2 border-black-500 px-4 py-2 mx-3 my-3 text-center">
+  Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description
+</div>
+<div>
+  <div class = "flow-text break-words w-6/12 max-w-max rounded-lg border-2 border-black-500 px-4 py-2 mx-3 my-3 text-center">
+    Daily Deals Daily Deals Daily Deals Daily Deals Daily Deals Daily Deals Daily Deals Daily Deals Daily Deals Daily Deals Daily Deals Daily Deals Daily Deals Daily Deals Daily Deals Daily Deals Daily Deals Daily Deals Daily Deals Daily Deals Daily Deals Daily Deals Daily Deals
+  </div>
+  <div class = "flow-text break-words w-6/12 max-w-max rounded-lg border-2 border-black-500 px-4 py-2 mx-3 my-3 text-center">
+    Daily Chest Daily Chest Daily Chest Daily Chest Daily Chest Daily Chest Daily Chest Daily Chest Daily Chest Daily Chest Daily Chest Daily Chest Daily Chest Daily Chest Daily Chest Daily Chest Daily Chest Daily Chest Daily Chest Daily Chest Daily Chest Daily Chest Daily Chest
+  </div>
 </div>
 `,
   standalone: true,
