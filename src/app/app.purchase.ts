06503f029a87b6a0dd8c6fa60b2e4c122dadd795
AntSwift1 <antswift10@gmail.com>
2024-06-16 02:18:34 -0400

    Began work on the purchase page

--- src/app/app.purchase.ts
+++ src/app/app.purchase.ts
@@ -5,7 +5,14 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-purchase',
   template: `
-Purchase page
+  <p class = "flex flex-col items-center align-middle px-auto py-2">Category A</p>
+<div class = "flow-text break-words w-xl max-w-max rounded-lg border-2 border-black-500 px-4 py-1 mx-3 my-3 text-center">
+  Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff
+</div>
+<p class = "flex flex-col items-center align-middle px-auto py-2">Category B</p>
+<div class = "flow-text break-words w-xl max-w-max rounded-lg border-2 border-black-500 px-4 py-1 mx-3 my-3 text-center">
+  Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff Stuff
+</div>
 `,
   standalone: true,
   styleUrl: './app.component.css'
