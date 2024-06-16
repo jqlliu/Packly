3be8bf2e7d5f2ab4e460d9dcdd50105fd94855a2
AntSwift1 <antswift10@gmail.com>
2024-06-16 03:14:33 -0400

    Swapped out some wording

--- src/app/app.home.ts
+++ src/app/app.home.ts
@@ -10,42 +10,42 @@ import { RouterOutlet } from '@angular/router';
   Welcome to [Name]! Check in daily to get some [Currency] from the daily chest! Use your [Currency] to buy packs of cards, which give you a random assortment of cards, based off of the pack. You can also trade these cards with other users in the 'Trade' tab, and see your collection in the 'Your Items' tab. Of course, you'll need to login to start. Make an account now by pressing the 'Login' tab!
 </div>
 <div class = "flex w-full">
-  <ul class="overflow-x-auto inline-block flow-text break-words w-1/2 rounded-lg border-2 border-black-500 px-4 py-2 mx-2 flex space-x-4">
+  <ul class="overflow-x-auto break-words w-1/2 rounded-lg border-2 border-black-500 px-4 py-2 mx-2 flex space-x-4">
     <li class="inline-block flow-text break-words w-md max-w-max rounded-lg border-2 border-black-500 px-4 py-2 mx-2 text-center">
-      Item 1
+      Daily Deal 1
     </li>
     <li class="inline-block flow-text break-words w-md max-w-max rounded-lg border-2 border-black-500 px-4 py-2 mx-2 text-center">
-      Item 2
+      Daily Deal 2
     </li>
     <li class="inline-block flow-text break-words w-md max-w-max rounded-lg border-2 border-black-500 px-4 py-2 mx-2 text-center">
-      Item 3
+      Daily Deal 3
     </li>
     <li class="inline-block flow-text break-words w-md max-w-max rounded-lg border-2 border-black-500 px-4 py-2 mx-2 text-center">
-      Item 4
+      Daily Deal 4
     </li>
     <li class="inline-block flow-text break-words w-md max-w-max rounded-lg border-2 border-black-500 px-4 py-2 mx-2 text-center">
-      Item 5
+      Daily Deal 5
     </li>
     <li class="inline-block flow-text break-words w-md max-w-max rounded-lg border-2 border-black-500 px-4 py-2 mx-2 text-center">
-      Item 6
+      Daily Deal 6
     </li>
     <li class="inline-block flow-text break-words w-md max-w-max rounded-lg border-2 border-black-500 px-4 py-2 mx-2 text-center">
-      Item 7
+      Daily Deal 7
     </li>
     <li class="inline-block flow-text break-words w-md max-w-max rounded-lg border-2 border-black-500 px-4 py-2 mx-2 text-center">
-      Item 8
+      Daily Deal 8
     </li>
     <li class="inline-block flow-text break-words w-md max-w-max rounded-lg border-2 border-black-500 px-4 py-2 mx-2 text-center">
-      Item 9
+      Daily Deal 9
     </li>
     <li class="inline-block flow-text break-words w-md max-w-max rounded-lg border-2 border-black-500 px-4 py-2 mx-2 text-center">
-      Item 10
+      Daily Deal 10
     </li>
     <li class="inline-block flow-text break-words w-md max-w-max rounded-lg border-2 border-black-500 px-4 py-2 mx-2 text-center">
-      Item 11
+      Daily Deal 11
     </li>
     <li class="inline-block flow-text break-words w-md max-w-max rounded-lg border-2 border-black-500 px-4 py-2 mx-2 text-center">
-      Item 12
+      Daily Deal 12
     </li>
   </ul>
   <div class = "inline-block flow-text break-words w-1/2 rounded-lg border-2 border-black-500 px-4 py-2 mx-2 text-center">
