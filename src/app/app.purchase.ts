8f4be32c97d3e02fe9e77ff5bd06537ca298d6c0
AntSwift1 <antswift10@gmail.com>
2024-06-16 02:31:12 -0400

    Redid purchase using lists instead

--- src/app/app.purchase.ts
+++ src/app/app.purchase.ts
@@ -6,45 +6,38 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-purchase',
   template: `
   <p class = "flex flex-col items-center align-middle px-auto py-2">Category A</p>
-<div class = "flow-text break-words w-xl max-w-max rounded-lg border-2 border-black-500 px-4 py-1 mx-3 my-3 text-center">
-  <div class = "flex items-center justify-center py-2 mx-2">
-    <div class = "inline-block flow-text break-words w-md max-w-max rounded-lg border-2 border-black-500 px-4 py-2 mx-2 text-center">
+  <ul class="flow-text break-words w-xl max-w-max rounded-lg border-2 border-black-500 px-4 py-2 mx-3 my-3 text-center flex flex-col sm:flex-row">
+    <li class="inline-block flow-text break-words w-md max-w-max rounded-lg border-2 border-black-500 px-4 py-2 mx-2 text-center">
       Item 1
-    </div>
-    <div class = "inline-block flow-text break-words w-md max-w-max rounded-lg border-2 border-black-500 px-4 py-2 mx-2 text-center">
+    </li>
+    <li class="inline-block flow-text break-words w-md max-w-max rounded-lg border-2 border-black-500 px-4 py-2 mx-2 text-center">
       Item 2
-    </div>
-    <div class = "inline-block flow-text break-words w-md max-w-max rounded-lg border-2 border-black-500 px-4 py-2 mx-2 text-center">
+    </li>
+    <li class="inline-block flow-text break-words w-md max-w-max rounded-lg border-2 border-black-500 px-4 py-2 mx-2 text-center">
       Item 3
-    </div>
-    <div class = "inline-block flow-text break-words w-md max-w-max rounded-lg border-2 border-black-500 px-4 py-2 mx-2 text-center">
+    </li>
+    <li class="inline-block flow-text break-words w-md max-w-max rounded-lg border-2 border-black-500 px-4 py-2 mx-2 text-center">
       Item 4
-    </div>
-    <div class = "inline-block flow-text break-words w-md max-w-max rounded-lg border-2 border-black-500 px-4 py-2 mx-2 text-center">
+    </li>
+    <li class="inline-block flow-text break-words w-md max-w-max rounded-lg border-2 border-black-500 px-4 py-2 mx-2 text-center">
       Item 5
-    </div>
-    <div class = "inline-block flow-text break-words w-md max-w-max rounded-lg border-2 border-black-500 px-4 py-2 mx-2 text-center">
+    </li>
+    <li class="inline-block flow-text break-words w-md max-w-max rounded-lg border-2 border-black-500 px-4 py-2 mx-2 text-center">
       Item 6
-    </div>
-  </div>
-</div>
-<p class = "flex flex-col items-center align-middle px-auto py-2">Category B</p>
-<div class = "flow-text break-words w-xl max-w-max rounded-lg border-2 border-black-500 px-4 py-1 mx-3 my-3 text-center">
-  <div class = "flex items-center justify-center py-2 mx-2">
-    <div class = "inline-block flow-text break-words w-md max-w-max rounded-lg border-2 border-black-500 px-4 py-2 mx-2 text-center">
+    </li>
+  </ul>
+  <p class = "flex flex-col items-center align-middle px-auto py-2">Category B</p>
+  <ul class="flow-text break-words w-xl max-w-max rounded-lg border-2 border-black-500 px-4 py-2 mx-3 my-3 text-center flex flex-col sm:flex-row">
+    <li class="inline-block flow-text break-words w-md max-w-max rounded-lg border-2 border-black-500 px-4 py-2 mx-2 text-center">
       Item 1
-    </div>
-    <div class = "inline-block flow-text break-words w-md max-w-max rounded-lg border-2 border-black-500 px-4 py-2 mx-2 text-center">
+    </li>
+    <li class="inline-block flow-text break-words w-md max-w-max rounded-lg border-2 border-black-500 px-4 py-2 mx-2 text-center">
       Item 2
-    </div>
-    <div class = "inline-block flow-text break-words w-md max-w-max rounded-lg border-2 border-black-500 px-4 py-2 mx-2 text-center">
+    </li>
+    <li class="inline-block flow-text break-words w-md max-w-max rounded-lg border-2 border-black-500 px-4 py-2 mx-2 text-center">
       Item 3
-    </div>
-    <div class = "inline-block flow-text break-words w-md max-w-max rounded-lg border-2 border-black-500 px-4 py-2 mx-2 text-center">
-      Item 4
-    </div>
-  </div>
-</div>
+    </li>
+  </ul>
 `,
   standalone: true,
   styleUrl: './app.component.css'
