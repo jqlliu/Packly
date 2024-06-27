c875d9bef6ccd5b2845ccaa23cdc79c4cf494f18
AntSwift1 <antswift10@gmail.com>
2024-06-27 00:50:56 -0400

    Added API stiff to account, and added more fields to account

--- src/app/app.account.ts
+++ src/app/app.account.ts
@@ -7,8 +7,24 @@ import { ApiService } from './api.service';
 @Component({
   selector: 'app-account',
   template: `
-  <p class = "mx-5 my-5">Username: {{username}}</p>
-  <p class = "mx-5 my-5">E-Mail: {{email}}</p>
+  <p class = "mx-5 my-3">General Info</p>
+  <p class = "mx-5 my-3">Username: {{username}}</p>
+  <p class = "mx-5 my-3">E-Mail: {{email}}</p>
+  <p class = "mx-5 my-3">Current Balance: {{username}}</p>
+  <p class = "mx-5 my-3">Account Creation Date: {{username}}</p>
+
+  <p class = "mx-5 my-3 mt-10">Trades</p>
+  <p class = "mx-5 my-3">Outgoing Trades: {{username}}</p>
+  <p class = "mx-5 my-3">Ingoing Trades: {{username}}</p>
+
+  <p class = "mx-5 my-3 mt-10">Account Stats</p>
+  <p class = "mx-5 my-3">Total Cards Owned: {{username}} / {{username}}</p>
+  <p class = "mx-5 my-3">Total Cards Collected: {{username}} / {{username}}</p>
+  <p class = "mx-5 my-3">Rarity 1 Cards Collected: {{username}} / {{username}}</p>
+  <p class = "mx-5 my-3">Rarity 2 Cards Collected: {{username}} / {{username}}</p>
+  <p class = "mx-5 my-3">Rarity 3 Cards Collected: {{username}} / {{username}}</p>
+  <p class = "mx-5 my-3">Chests Opened: {{username}}</p>
+  <p class = "mx-5 my-3">Packs Opened: {{username}}</p>
 `,
   standalone: true,
   styleUrl: './app.component.css'
@@ -18,16 +34,16 @@ import { ApiService } from './api.service';
 
 export class AccountComponent implements OnInit {
   title = 'Account';
+  username = '';
+  email = '';
   constructor(private api: ApiService){
   }
   ngOnInit(): void {
-    this.api.get().subscribe(data => {
+    this.api.getAccountInfo(1).subscribe( (data: any) => {
+      console.log(data.username);
       console.log(data);
-    });
-    this.api.getAccountInfo({id: 1}).subscribe(data => {
-      console.log(data);
+      this.username = data.username;
+      this.email = data.email;
     })
   }
-  username = 'TestGuy';
-  email = 'TestGuy@Test.Com';
 }
