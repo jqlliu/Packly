9313d4dcc5fe7d01212c9dbc1fc5321167ca9313
AntSwift1 <antswift10@gmail.com>
2024-09-01 19:40:22 -0400

    added test data

--- src/app/app.home.ts
+++ src/app/app.home.ts
@@ -12,6 +12,7 @@ import { firstValueFrom } from 'rxjs';
   <div class = "flow-text break-words w-xl max-w-max rounded-lg border-2 border-black-500 px-4 py-2 mx-3 my-3 text-center">
     Welcome to Packly! Check in daily to get some Coins from the daily chest! Use your Coins to buy packs of cards, which give you a random assortment of cards, based off of the pack. You can also trade these cards with other users in the 'Trade' tab, and see your collection in the 'Your Items' tab. Of course, you'll need to login to start. Make an account now by pressing the 'Login' tab!
   </div>
+  <div *ngIf="serverTime">Server Time: {{ serverTime }}</div>
   <div class = "flex w-full">
     <div class="overflow-x-auto break-words w-1/2 rounded-lg border-2 border-black-500 px-4 py-2 mx-2 flex space-x-4">
       <div class="inline-block flow-text break-words rounded-lg border-2 border-black-500 px-4 py-2 mx-2 text-center" *ngFor="let id of numberArray(12)">
@@ -32,12 +33,20 @@ import { firstValueFrom } from 'rxjs';
 
 export class HomeComponent {
   title = 'Home';
+  serverTime: string | undefined;
 
   constructor(private apiService: ApiService) {
+    this.getServerTime();
   }
 
   //I stole this from the internet lol
   numberArray(length: number): number[] {
     return Array.from({ length }, (_, i) => i);
+  }
+
+  getServerTime() {
+    this.apiService.getTime().subscribe((data: any) => {
+      this.serverTime = data.time;
+    });
   }
 }
