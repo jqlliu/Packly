fcdaf401964f8816c738487cc91ba1b15d9e6c09
AntSwift1 <antswift10@gmail.com>
2024-09-02 11:39:52 -0400

    added routeguard logic

--- src/app/api.routeguard.ts
+++ src/app/api.routeguard.ts
@@ -1,8 +1,28 @@
-import { Injectable } from "@angular/core";
+import { inject, Injectable } from "@angular/core";
 import { HttpClient, HttpParams } from "@angular/common/http";
-import {  ActivatedRouteSnapshot, CanActivateFn, RouterStateSnapshot} from "@angular/router";
+import {  ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot} from "@angular/router";
 import { CookieService } from 'ngx-cookie-service';
 
 export const canActivate: CanActivateFn = (next: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+    const router = inject(Router);
+    const cookieService = inject(CookieService);
+
+    const sessionKey = cookieService.get('sessionKey');
+    const needsLogin = next.data['needsLogin'] ?? false;
+    const needsLogout = next.data['needsLogout'] ?? false;
+
+    //If you need a login, but aren't, route to Login
+    if (needsLogin && !sessionKey) {
+        router.navigate(['/login']);
+        return false;
+    }
+
+    //If you need to be logged out, but are, route to Home
+    if (needsLogout && sessionKey) {
+        router.navigate(['/']);
+        return false;
+    }
+
+    //Didn't hit a break, allow navigation
     return true;
 };
