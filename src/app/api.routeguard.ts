b16c31b1458471332e7bb37b4e57934b09960b0e
AntSwift1 <antswift10@gmail.com>
2024-09-02 10:48:56 -0400

    Gave routeguard cookie service

--- src/app/api.routeguard.ts
+++ src/app/api.routeguard.ts
@@ -1,9 +1,8 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpParams } from "@angular/common/http";
 import {  ActivatedRouteSnapshot, CanActivateFn, RouterStateSnapshot} from "@angular/router";
+import { CookieService } from 'ngx-cookie-service';
 
-export  const canActivate: CanActivateFn = (
-    next: ActivatedRouteSnapshot, state: RouterStateSnapshot
-    ) =>{
-        return true;
-    };
+export const canActivate: CanActivateFn = (next: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+    return true;
+};
