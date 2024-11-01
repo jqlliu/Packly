63de4ad3894b3735ca5780b12c5f5bd75247ef03
AntSwift1 <antswift10@gmail.com>
2024-10-31 22:59:02 -0400

    fixed let bug

--- server.js
+++ server.js
@@ -96,17 +96,18 @@ function drawCards(packName, doesHave, rarities) {
         //Type 1 computation
         //I love you internet. This filters to all the valid cards, so you dont have it, and it's of the right rarity.
         let valid = cards.filter((_, index) => rarities[index] === rarity && doesHave[index] === 0);
+        let drawnCard;
         if (valid.length() == 0) {
           //Already have all cards of this rarity.
           valid = cards.filter((_, index) => rarities[index] === rarity);
-          let drawnCard = valid[Math.floor(Math.random() * valid.length)];
+          drawnCard = valid[Math.floor(Math.random() * valid.length)];
         } else {
-          let drawnCard = valid[Math.floor(Math.random() * valid.length)];
+          drawnCard = valid[Math.floor(Math.random() * valid.length)];
         }
       } else {
         //Type 0 computation
         valid = cards.filter((_, index) => rarities[index] === rarity);
-        let drawnCard = valid[Math.floor(Math.random() * valid.length)];
+        drawnCard = valid[Math.floor(Math.random() * valid.length)];
       }
 
       //Add drwn card to array
